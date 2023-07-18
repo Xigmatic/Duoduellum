b31e07288c40fd1155412c9046b96218677a0e0d
Xigmatic <tncolton1@gmail.com>
2023-07-18 18:11:40 -0400

    Added startup message

--- dogfight/src/main/java/xigmatic/me/dogfight/Dogfight.java
+++ dogfight/src/main/java/xigmatic/me/dogfight/Dogfight.java
@@ -1,5 +1,6 @@
 package xigmatic.me.dogfight;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Dogfight extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class Dogfight extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage("DOGFIGHT PLUGIN IS NOW ENABLED");
     }
 
     @Override
